8512ba397cb59e928c162fbc8dc46fded047eb15
ruoningwen <ruoningwen@gmail.com>
2017-08-18 11:39:28 +0800

    update svm code

--- svm_train.py
+++ svm_train.py
@@ -3,6 +3,9 @@ import jieba
 from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
 import random
+from sklearn.decomposition import  PCA
+
+
 
 def svm_train_predict(cate_name):
     # percentage of training set in dataset
@@ -12,78 +15,97 @@ def svm_train_predict(cate_name):
 
     # load data
     pos_lines = open('./%s_pos.txt' % cate_name).readlines()
-    num_pos = len(pos_lines)
     neu_lines = open('./%s_neu.txt' % cate_name).readlines()
-    num_neu = len(neu_lines)
     neg_lines = open('./%s_neg.txt' % cate_name).readlines()
-    num_neg = len(neg_lines)
-    num_all = num_pos + num_neu + num_neg
-    print('num_pos: %s' % num_pos)
-    print('num_neu: %s' % num_neu)
-    print('num_neg: %s' % num_neg)
-    print('num_all: %s' % num_all)
-    print('percentage_pos: %s' % str(num_pos / num_all))
-    print('percentage_neu: %s' % str(num_neu / num_all))
-    print('percentage_neg: %s' % str(num_neg / num_all))
-    train_point_pos = int(num_pos * validation_split)
-    train_point_neu = int(num_pos + num_neu * validation_split)
-    train_point_neg = int(num_pos + num_neu + num_neg * validation_split)
 
     # build the dataset
     corpus = []
     y = []
-    for line in pos_lines:
-        corpus.append(' '.join(jieba.cut(line.replace('\n', ''))))
+    pos_corpus = []
+    neu_corpus = []
+    neg_corpus = []
+    for line in pos_lines[:330]:
+        pos_corpus.append(' '.join(jieba.cut(line.replace('\n', ''))))
         y.append(1)
-    for line in neu_lines:
-        corpus.append(' '.join(jieba.cut(line.replace('\n', ''))))
-        y.append(2)
-    for line in neg_lines:
-        corpus.append(' '.join(jieba.cut(line.replace('\n', ''))))
-        y.append(3)
+    for i in range(1):
+        for line in neu_lines[:330]:
+            neu_corpus.append(' '.join(jieba.cut(line.replace('\n', ''))))
+            y.append(2)
+    for i in range(1):
+        for line in neg_lines:
+            neg_corpus.append(' '.join(jieba.cut(line.replace('\n', ''))))
+            y.append(3)
+
+    corpus.extend(pos_corpus)
+    corpus.extend(neu_corpus)
+    corpus.extend(neg_corpus)
+
+    num_pos = len(pos_corpus)
+    num_neu = len(neu_corpus)
+    num_neg = len(neg_corpus)
+    num_all = len(corpus)
+    print('num_pos: %s' % num_pos)
+    print('num_neu: %s' % num_neu)
+    print('num_neg: %s' % num_neg)
+    print('num_all: %s' % num_all)
+    print('all_percentage_pos: %s' % str(num_pos / num_all))
+    print('all_percentage_neu: %s' % str(num_neu / num_all))
+    print('all_percentage_neg: %s' % str(num_neg / num_all))
+
+    random.Random(seed_random).shuffle(corpus)
+    random.Random(seed_random).shuffle(y)
 
     # transform the dataset to the bag_of_word format
     vectorizer = CountVectorizer(min_df=1)
-    X = vectorizer.fit_transform(corpus).toarray()
+    x = vectorizer.fit_transform(corpus).toarray()
     y = np.array(y)
-    X = np.array(X)
+    x = np.array(x)
+
+    # print('----> PCA <----')
+    # pca = PCA(n_components=50)
+    # x = pca.fit_transform(x)
 
     print('----> Shape of data set: <----')
-    print(X.shape)
-    print('----> Shape of label <----')
-    print(y.shape)
+    print(x.shape)
 
     # build training set and test set
-    x_train = []
-    y_train = []
-    x_train.extend(X[0:train_point_pos])
-    x_train.extend(X[num_pos:train_point_neu])
-    x_train.extend(X[num_pos + num_neu:train_point_neg])
+    x_train = x[:int(num_all * validation_split)]
+    y_train = y[:int(num_all * validation_split)]
 
-    y_train.extend(y[0:train_point_pos])
-    y_train.extend(y[num_pos:train_point_neu])
-    y_train.extend(y[num_pos + num_neu:train_point_neg])
+    num_train_pos = len(list(filter(lambda n: n == 1, y_train)))
+    num_train_neu = len(list(filter(lambda n: n == 2, y_train)))
+    num_train_neg = len(list(filter(lambda n: n == 3, y_train)))
 
-    random.Random(seed_random).shuffle(x_train)
-    random.Random(seed_random).shuffle(y_train)
+    print('num_train_pos', num_train_pos)
+    print('num_train_neu', num_train_neu)
+    print('num_train_neg', num_train_neg)
 
-    X_test = []
-    y_test = []
-    X_test.extend(X[train_point_pos:num_pos])
-    X_test.extend(X[train_point_neu:num_pos + num_neu])
-    X_test.extend(X[train_point_neg:num_pos + num_neu + num_neg])
+    x_test = x[int(num_all * validation_split):]
+    y_test = y[int(num_all * validation_split):]
+    print('Num of training set: %s' % len(y_train))
+    print('Num of test set: %s' % len(y_test))
 
-    y_test.extend(y[train_point_pos:num_pos])
-    y_test.extend(y[train_point_neu:num_pos + num_neu])
-    y_test.extend(y[train_point_neg:num_pos + num_neu + num_neg])
+    num_test_pos = len(list(filter(lambda n: n == 1, y_test)))
+    num_test_neu = len(list(filter(lambda n: n == 2, y_test)))
+    num_test_neg = len(list(filter(lambda n: n == 3, y_test)))
 
-    print('Num of training set: %s' % len(x_train))
+    print('num_test_pos', num_test_pos)
+    print('num_test_neu', num_test_neu)
+    print('num_test_neg', num_test_neg)
+    print('test_percentage_pos: %s' % str(num_test_pos / len(y_test)))
+    print('test_percentage_neu: %s' % str(num_test_neu / len(y_test)))
+    print('test_percentage_neg: %s' % str(num_test_neg / len(y_test)))
 
-    print('Num of test set: %s' % len(X_test))
-
-    clf = svm.SVC()
+    clf = svm.SVC(decision_function_shape='ovo')
     clf.fit(x_train, y_train)
-    y_predict = clf.predict(X_test)
+    y_predict = clf.predict(x_test)
+
+    num_predict_pos = len(list(filter(lambda n: n == 1, y_predict)))
+    num_predict_neu = len(list(filter(lambda n: n == 2, y_predict)))
+    num_predict_neg = len(list(filter(lambda n: n == 3, y_predict)))
+    print('num_predict_pos', num_predict_pos)
+    print('num_predict_neu', num_predict_neu)
+    print('num_predict_neg', num_predict_neg)
 
     # calculate the accuracy
     y_predict = np.array(y_predict)
@@ -94,16 +116,14 @@ def svm_train_predict(cate_name):
 
     print('Num of predict:')
     print(len(y_predict))
-
     print('Num of true predict:')
     print(len(predict_true))
-
     print('Accuracy:')
     print(accu)
 
 
 if __name__ == '__main__':
-    svm_train_predict('price')
+    svm_train_predict('food')
 
 
 
